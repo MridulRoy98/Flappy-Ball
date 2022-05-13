cb174d7f01482de315208213b937f9b9ae410fe4
Mridul Roy <69635889+MridulRoy98@users.noreply.github.com>
2022-05-13 18:02:17 +0600

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,10 +7,9 @@ public class Player : MonoBehaviour
 {
     private Rigidbody rb;
     public float force;
-    private int points = 0;
+    public int points;
  
-
-
+    //Adding Rigidbody to ball and adding gravity
     void Start()
     {
         rb = gameObject.AddComponent<Rigidbody>();
@@ -20,8 +19,12 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+
+        //Adding constant downward force which keep the player falling
         rb.AddForce(Vector3.down * 1.5f);
 
+
+        //Checking if there's a touch detected on screen
         if (Input.touchCount > 0) 
         {
             Touch touch = Input.GetTouch(0);
@@ -32,18 +35,23 @@ public class Player : MonoBehaviour
 
         }
 
+        //flap mechanic for testing in PC
         if (Input.GetKeyUp(KeyCode.Space))
         {
             PlayerMovement();
         }
 
     }
+
+    //Function for flap movement upward
     void PlayerMovement()
     {
         rb.velocity = Vector3.up * force;
 
     }
 
+
+    //Checking if the player touches the pillars and if they do the game is restarted
     private void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.gameObject.name == "Obstacles" || collisionInfo.gameObject.name == "Obstacles_one" || collisionInfo.gameObject.name == "Obstacles_two" || collisionInfo.gameObject.name == "Floor")
@@ -53,24 +61,27 @@ public class Player : MonoBehaviour
         
     }
 
+
+    //Checking if the player goes through the obstacles
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Detector" || other.gameObject.name == "Detector_one" || other.gameObject.name == "Detector_two")
         {
-            AddPoints();
+            //if the player crosses the detector between the pipes, they gain a point
+            points += 1;
         }
     }
 
-
+    //Restarts the game
     void RestartGame()
     {
         Debug.Log("You died");
         SceneManager.LoadScene(0);
     }
 
-    void AddPoints()
+    //Returns the current points to show in UI script
+    public int getPoints()
     {
-        points += 1;
-        Debug.Log("Points: " + points);
+        return points;
     }
 }
